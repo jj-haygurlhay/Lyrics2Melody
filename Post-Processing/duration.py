838d7726cd473d10db9d513b31af5a9efad274ce
Maxime Bélanger <mbelanger@locketgo.com>
2024-03-23 18:15:59 -0400

    Little bit more doc

--- Post-Processing/duration.py
+++ Post-Processing/duration.py
@@ -2,12 +2,24 @@ import numpy
 
 class rest_duration:
     DURATION=[0,1,2,4,8,16,32]
+    """
+    Possible rest durations:
+
+    [0,1,2,4,8,16,32]
+    """
     def quantize(durations):
+        """Quantize each duration to its closest allowed rest duration."""
         return [rest_duration.DURATION[numpy.abs(numpy.subtract(rest_duration.DURATION, duration)).argmin()] for duration in durations]
 
 class note_duration:
     DURATION=[0.25,0.5,0.75,1,1.5,2,3,4,6,8,16,32]
+    """
+    Possible note durations:
+
+    [0,1,2,4,8,16,32]
+    """
     def quantize(durations):
+        """Quantize each duration to its closest allowed note duration."""
         return [note_duration.DURATION[numpy.abs(numpy.subtract(note_duration.DURATION, duration)).argmin()] for duration in durations]
 
 
