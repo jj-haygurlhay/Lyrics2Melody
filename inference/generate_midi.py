79b114a7c28226b02fa8ffd28f03a685fa2933b2
j_haygurlhay_j <jj.kaufman1@gmail.com>
2024-03-26 10:12:15 -0400

    midis
    midis

--- inference/generate_midi.py
+++ inference/generate_midi.py
@@ -1,36 +1,25 @@
-import torch
-import torch.nn as nn
-import mido
+from utils import midi_utils
 
-class GenerateMidi(nn.Module):
+class GenerateMidi:
     def __init__(self, decoded_notes, decoded_durations, decoded_gaps):
         self.decoded_notes = decoded_notes
         self.decoded_durations = decoded_durations
         self.decoded_gaps = decoded_gaps
-
-    def create_MIDI(self):
-        mid = mido.MidiFile()
-        track = mido.MidiTrack()
-        mid.tracks.append(track)
+    
+    def create_midi(self, bpm=120, title="", copyright=""):
+        mid = midi_utils.create_new_midi()  # Assuming midi_utils has this function
+        midi_utils.set_tempo(mid, bpm)
+        midi_utils.add_metadata(mid, title, copyright)
         
-        # Set an initial delay if necessary
-        track.append(mido.MetaMessage('set_tempo', tempo=mido.bpm2tempo(120)))
+        track = midi_utils.create_track(mid)  # Or however you create a track in midi_utils
         
-        # Iterate over the notes and durations to create MIDI messages
         for note, duration, gap in zip(self.decoded_notes, self.decoded_durations, self.decoded_gaps):
-            # Add a note on
-            track.append(mido.Message('note_on', note=note, velocity=64, time=0))
-            # Add a note off, note the time here is the duration
-            track.append(mido.Message('note_off', note=note, velocity=64, time=duration))
-            # Add a gap if necessary
-            if gap:
-                track.append(mido.Message('note_off', note=note, velocity=0, time=gap))
+            midi_utils.add_note(track, note, duration, gap)
         
         return mid
     
     def save_midi(self, filename):
-        # Assumes that a MIDI file has been created with create_midi
         mid = self.create_midi()
-        mid.save(filename)
+        midi_utils.save_midi(mid, filename)
         print(f'MIDI file saved as {filename}')
 
