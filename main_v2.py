553101a52a2594724c64f9f6f0de0468ba0902c3
Guillaume Charron <guillaume.charron99@hotmail.com>
2024-04-01 16:30:52 -0400

    Still trashy results

--- main_v2.py
+++ main_v2.py
@@ -3,7 +3,7 @@ import yaml
 import torch
 from torch.utils.data import DataLoader
 from models import MusicT5, MusicGPT2
-from datasets import load_dataset
+from datasets import load_dataset, load_metric
 from training import Trainer as CustomTrainer
 # from pytorch_lightning import Trainer as PLTrainer
 from transformers import (
@@ -14,13 +14,20 @@ from transformers import (
     DataCollatorForSeq2Seq,
     Seq2SeqTrainingArguments,
     Seq2SeqTrainer, 
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    GenerationConfig,
     set_seed
 )
 from dataloader import SongsDataset, SongsCollator
-from utils.quantize import encode_note, encode_duration, encode_gap
+from utils.quantize import encode_note, encode_duration, encode_gap, MIDI_NOTES, DURATIONS, GAPS
+import numpy as np
+import nltk
+from datetime import datetime
 
 HYPS_FILE = './config/hyps.yaml'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 def serialize_melody(midi_seq):
     """
     Serialize MIDI sequence to a string format for tokenization.
@@ -28,9 +35,9 @@ def serialize_melody(midi_seq):
     """
     serialized_seq = []
     for note, duration, gap in midi_seq:
-        note_token = f"note {encode_note(note)}"
-        duration_token = f"duration {encode_duration(duration)}"
-        gap_token = f"gap {encode_gap(gap)}"
+        note_token = f"<note{encode_note(note)}>"
+        duration_token = f"<duration{encode_duration(duration)}>"
+        gap_token = f"<gap{encode_gap(gap)}>"
         serialized_seq.append(f"{note_token} {duration_token} {gap_token}")
     return " ".join(serialized_seq)
 
@@ -41,49 +48,34 @@ def main():
     # Set seed
     set_seed(config['seed'])
 
-    # pl_trainer = PLTrainer()
 
     # Load model and tokenizer
-    model_name = config['model']['model_name']
-    del config['model']['model_name']
+    model_name = 't5-small'
+    t5_config = T5Config.from_pretrained(model_name)
+    gen_config = GenerationConfig.from_model_config(t5_config)
+    #gen_config.max_length = config['data']['max_sequence_length']
 
-    if 't5' in model_name:
-        t5_config = T5Config.from_pretrained(
-            't5-small'
-            )
-        # print(t5_config)
-        model = MusicT5(t5_config, **config['model']) # TODO create config object
-        tokenizer = T5Tokenizer.from_pretrained(pretrained_model_name_or_path='t5-small')
-    elif 'gpt' in model_name:
-        gpt2_config = GPT2Config.from_pretrained('gpt2')
-        model = MusicGPT2(gpt2_config, **config['model']) # TODO create config object
-        tokenizer = GPT2Tokenizer.from_pretrained(pretrained_model_name_or_path='gpt2')
-        tokenizer.padding_side = "left"
-        tokenizer.pad_token = tokenizer.eos_token
-    else:
-        raise ValueError("Model not implemented")
 
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    model.generation_config = gen_config
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.add_tokens([f'<note{i}>' for i in range(len(MIDI_NOTES))])
+    tokenizer.add_tokens([f'<duration{i}>' for i in range(len(DURATIONS))])
+    tokenizer.add_tokens([f'<gap{i}>' for i in range(len(GAPS))])
+    model.resize_token_embeddings(len(tokenizer))
     model.to(device)
 
     # Create dataset and collator
     batch_size = config['training']['batch_size']
 
     #train_dataset = SongsDataset(config['data']['data_dir'], split='train')
-    train_dataset = load_dataset('csv', data_files={'train': 'data/new_dataset/train.csv', 'valid': 'data/new_dataset/valid.csv', 'test': 'data/new_dataset/test.csv'})
-    valid_dataset = SongsDataset(config['data']['data_dir'], split='valid')
-    test_dataset  = SongsDataset(config['data']['data_dir'], split='test')
-    collator = SongsCollator(tokenizer=tokenizer, output_eos=config['data']['output_eos'], max_length=config['data']['max_sequence_length'], use_syllables=config['data']['use_syllables'])
-    
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collator, pin_memory=True, num_workers=4, persistent_workers=True)
-    val_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True, collate_fn=collator, pin_memory=True, num_workers=4, persistent_workers=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, collate_fn=collator, pin_memory=True, num_workers=4, persistent_workers=True)
-
+    dataset = load_dataset('csv', data_files={'train': 'data/new_dataset/train.csv', 'valid': 'data/new_dataset/valid.csv', 'test': 'data/new_dataset/test.csv'})
 
     def preprocess_function(examples):
         
-        inputs = examples['lyrics']
+        inputs = ['Generate notes: ' + lyrics for lyrics in examples['lyrics']]
         targets = examples['midi_notes']
-        results = tokenizer(inputs, return_tensors='pt', padding=True, truncation=True, max_length=config['data']['max_sequence_length'])
+        results = tokenizer(inputs, truncation=True, max_length=config['data']['max_sequence_length'])
         target_texts = []
 
         for item in targets:
@@ -91,64 +83,81 @@ def main():
             midi_seq = json.loads(item)[:config['data']['max_sequence_length']]
             serialized_melody = serialize_melody(midi_seq)
             target_texts.append(serialized_melody)
-        targets_encoding = tokenizer(target_texts, return_tensors='pt', padding='max_length', truncation=True, max_length=config['data']['max_sequence_length'])
+        targets_encoding = tokenizer(target_texts, truncation=True, max_length=config['data']['max_sequence_length'])
 
         results['labels'] = targets_encoding['input_ids']
-
         return results
 
-    train_dataset = train_dataset.map(preprocess_function, batched=True)
+    dataset = dataset.map(preprocess_function, batched=True)
     data_collator = DataCollatorForSeq2Seq(tokenizer)
 
-    # pl_trainer.fit(model, tokenizer, train_loader, val_loader, test_loader)
-
     # Create trainer
-    # custom_trainer = CustomTrainer(
-    #     model=model, 
-    #     pl_trainer=None,
-    #     device=device,
-    #     train_loader=train_loader,
-    #     val_loader=val_loader,
-    #     test_loader=test_loader,
-    #     collator=collator,
-    #     **config['training']
-    # )
-
-
-
+    now = datetime.now()
+    out_dir = f'./runs/{now.strftime("%Y-%m-%d_%H-%M-%S")}'
     args = Seq2SeqTrainingArguments(
-        './runs',
-        evaluation_strategy="steps",
-        eval_steps=500,
+        out_dir,
+        logging_dir= out_dir,
+        evaluation_strategy="epoch",
         logging_strategy="steps",
-        logging_steps=500,
-        save_strategy="steps",
-        save_steps=50000,
-        learning_rate=1e-4,
+        logging_steps=200,
+        save_strategy="epoch",
+        learning_rate=2e-5,
+        warmup_steps=0,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
-        weight_decay=0,
-        save_total_limit=0,
-        num_train_epochs=1,
+        weight_decay=0.01,
+        save_total_limit=3,
+        num_train_epochs=3,
         predict_with_generate=True,
         fp16=True,
         load_best_model_at_end=True,
         metric_for_best_model="rouge1",
-        report_to="tensorboard"
+        report_to="tensorboard",
     )
+
+    metric = load_metric("rouge")
+
+    def compute_metrics(eval_pred):
+        predictions, labels = eval_pred
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        
+        # Replace -100 in the labels as we can't decode them.
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+        
+        # Rouge expects a newline after each sentence
+        decoded_preds = ["\n".join(nltk.sent_tokenize(pred.strip()))
+                        for pred in decoded_preds]
+        decoded_labels = ["\n".join(nltk.sent_tokenize(label.strip())) 
+                        for label in decoded_labels]
+        
+        # Compute ROUGE scores
+        result = metric.compute(predictions=decoded_preds, references=decoded_labels,
+                                use_stemmer=True)
+
+        # Extract ROUGE f1 scores
+        result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+        
+        # Add mean generated length to metrics
+        prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id)
+                        for pred in predictions]
+        result["gen_len"] = np.mean(prediction_lens)
+        
+        return {k: round(v, 4) for k, v in result.items()}
+
     custom_trainer = Seq2SeqTrainer(
         model=model,
         args=args,
         tokenizer=tokenizer,
         data_collator=data_collator,
-        train_dataset=train_dataset['train'],
-        eval_dataset=train_dataset['valid'],
+        train_dataset=dataset['train'],
+        eval_dataset=dataset['valid'],
+        compute_metrics=compute_metrics
     )
 
     # Train model
     custom_trainer.train()
-
-    torch.save(model.state_dict(), 'model.pth')
-
+    custom_trainer.save_model()
+    custom_trainer.save_metrics()
 if __name__ == "__main__":
     main()
