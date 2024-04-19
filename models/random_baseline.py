5dc90ed151b86fb4198425c2cbc375c4e176e7ec
j_haygurlhay_j <jj.kaufman1@gmail.com>
2024-04-19 16:53:49 -0400

    final edits to random_baseline class

--- models/random_baseline.py
+++ models/random_baseline.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class RandomBaselineModel:
-    def __init__(self, midi_note_distribution, duration_distribution, gap_distribution, sequence_length=20):
+    def __init__(self, midi_note_distribution, duration_distribution, gap_distribution):
         """
         Initializes the RandomBaselineModel with separate distributions for MIDI notes, durations, and gaps.
         :param midi_note_distribution: A list or array with the probabilities for each MIDI note.
@@ -12,16 +12,18 @@ class RandomBaselineModel:
         self.midi_note_distribution = midi_note_distribution
         self.duration_distribution = duration_distribution
         self.gap_distribution = gap_distribution
-        self.sequence_length = sequence_length
+        self.sequence_length = None
         
         self.durations_array = np.array([0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 6.0, 6.5, 8.0, 8.5, 16.0, 16.5, 32.0, 32.5])
         self.gaps_array = np.array([0.0, 0.5, 1.0, 2.0, 4.0, 8.0, 16.0, 32.0])
 
-    def generate_melody(self):
+    def generate_melody(self, sequence_length = 20):
         """
         Generates a melody sequence based on the specified distributions.
         :return: A list of lists representing the melody with each inner list containing [MIDI note, duration, gap].
         """
+        self.sequence_length = sequence_length
+
         melody = []
         
         for _ in range(self.sequence_length):
